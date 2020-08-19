7e97eb88721f15d8153dad45ee96a5a214e41088
alexis <alexis.martin@unistellaroptics.com>
2020-08-20 01:46:08 +0200

    mostly done

---
+++ config/aliases.sh
@@ -0,0 +1,211 @@
+export EDITOR='emacs --no-window'
+alias rm='trash'
+alias ealias='emacs --no-window ~/.aliases.sh'
+alias sshlist='cat ~/.ssh/config'
+alias in='sudo apt-get install -y'
+alias out='sudo apt-get purge'
+alias q='exit'
+alias up='sudo apt-get update && sudo apt-get upgrade'
+alias d='wget -c'
+alias c='clear'
+alias cl='sudo apt-get clean;sudo apt-get -y autoremove --purge; sudo apt-get -y purge `deborphan`'
+alias m='mount | column -t'
+alias l='ls -lAh --color'
+alias sl='sudo ls -lah --color'
+alias ll='ls -lAh --color'
+alias ls='ls --color'
+alias lt='ls -t'
+alias h='htop'
+alias df='df -h'
+alias grep='grep --color'
+alias ga='git add -A'
+alias gs='git status'
+alias gb='git branch'
+alias gbv='git branch -avv'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias gdn='git diff --name-only'
+alias gdv='git difftool --tool=vimdiff --no-prompt'
+alias gl='git log'
+alias gln='git log --name-only'
+alias gll='git log --graph --oneline --decorate --all --remotes=origin'
+alias gc='git checkout'
+alias gp='git pull --recurse-submodules'
+alias gg='git grep -i'
+alias gcp='git cherry-pick'
+alias gr='git remote -v'
+alias gf='git fetch -p'
+alias enw='emacs --no-window'
+alias e='emacs'
+alias t='tree -Ca -I ".git*" --noreport'
+alias pd='qpdfview > /dev/null 2>&1'
+alias lsbig='find . -type f -print0 | xargs -0 du -h | sort -rh | head -n 10'
+alias beep='aplay -q /usr/share/orage/sounds/Spo.wav > /dev/null 2>&1'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias hs='history | tail -n50'
+alias s='sudo !!'
+alias xp='xbacklight +10'
+alias xm='xbacklight -10'
+alias dm='sudo dmesg -cHw'
+alias bw='wget http://test-debit.free.fr/image.iso -O /dev/null'
+alias sudo='sudo '
+alias n='sudo netstat -lptnu'
+alias si='sudo ifconfig'
+alias afs='apt-file search'
+alias b='watch -n1'
+alias pt='ping -c1 google.de'
+alias cc='sync; echo 3 | sudo tee /proc/sys/vm/drop_caches'
+alias dds='sudo pkill -USR1 dd'
+alias setenv='setpythonenv python-3'
+
+function res(){
+  if [ $6 ] ; then
+    width1="$(echo $2 | cut -dx -f1)"
+    width2="$(echo $4 | cut -dx -f1)"
+    (( width2 += width1 ))
+    xrandr --output $1 --mode $2 --pos 0x0 --output $3 --mode $4 --pos ${width1}x0 --output $5 --mode $6 --pos ${width2}x0
+  else
+    if [ $4 ] ; then
+      width="$(echo $2 | cut -dx -f1)" 
+      xrandr --output $1 --mode $2 --primary --pos 0x0 --output $3 --mode $4 --pos ${width}x0
+    else
+      if [ $2 ] ; then
+	xrandr --output $1 --mode $2
+      else
+	if [ $1 ] ; then
+	  xrandr --output eDP-1 --mode $1
+	else
+	  xrandr
+	fi
+      fi
+    fi
+  fi
+}
+
+function connect(){
+    if [ $1 ] ; then
+        sudo dhclient -v $1
+    else
+        sudo dhclient -v enx106530018c34
+    fi
+}
+
+function wifi(){
+  if [ $1 ] ; then
+    nmcli d wifi connect $1 password $2 iface wlp2s0
+  else
+    sudo iwlist wlp2s0 scan | grep ESSID
+  fi
+
+}
+
+function mkc() {
+  mkdir -p $1 && cd $1
+}
+
+function debchange () {
+  zless "/usr/share/doc/$1/changelog.Debian.gz"
+}
+
+function f () {
+find -type f -iname "*$**" -or -type d -iname "*$**" -and -not -path "*.git*" 2>/dev/null | egrep -i --color "$*"
+}
+
+function un () {
+  if [ -f $1 ] ; then
+    case $1 in
+      *.tar.bz2)   tar -I lbzip2 -xf $1     ;;
+      *.tar.gz)    tar -I pigz -xf $1     ;;
+      *.tar.xz)    tar -xf $1     ;;
+      *.tgz)    	 tar -I pigz -xf $1     ;;
+      *.bz2)       pbzip2 -d $1   ;;
+      *.rar)       unrar e $1     ;;
+      *.gz)        gunzip $1      ;;
+      *.tar)       tar xf $1      ;;
+      *.tbz2)      tar xjf $1     ;;
+      *.tgz)       tar xzf $1     ;;
+      *.zip)       unzip $1       ;;
+      *.Z)         uncompress $1  ;;
+      *.7z)        7z x $1        ;;
+      *.lzma)      lzma -d -k $1  ;;
+      *)     echo "'$1' cannot be extracted ()" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
+}
+
+function cz () {
+  tar -I pigz -cf "`basename $1`.tar.gz" $1
+}
+
+function databackup () {
+  function exit_backup() {
+    echo "** Trapped CTRL-C"
+    sudo umount /media/backup
+  }
+  #trap exit_backup INT
+  sudo fsck -y /dev/disk/by-label/data_backup
+  sudo mount /media/backup &&\
+    rsync -av --info=progress2 --exclude 'lost+found' --exclude '.Trash-*' --delete-after /media/data/ /media/backup/ &&\
+    sync &&\
+    sudo umount /media/backup
+}
+
+function srvsave () {
+  rsync --delete-after --rsync-path="sudo rsync" -aAX --info=progress2 --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} srv.sav:/ /media/data/info/os/srv-backup
+}
+
+function kc () {
+  sudo apt-get purge $(for tag in "linux-image" "linux-headers"; do dpkg-query -W -f'${Package}\n' "$tag-[0-9]*.[0-9]*.[0-9]*" | sort -V | awk 'index($0,c){exit} //' c=$(uname -r | cut -d- -f1,2); done)
+}
+
+function vl() {
+  dir=$(realpath "$1")
+  editor "$dir"/$(ls -t $dir | head -1)
+}
+
+function la() {
+  dir=$(realpath "$1")
+  echo "$dir"/$(ls -t $dir | head -1)
+}
+
+function naton() {
+  sudo iptables -t nat -A POSTROUTING -o "$1" -j MASQUERADE && echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
+}
+
+function natoff() {
+  sudo iptables -t nat -D POSTROUTING -o "$1" -j MASQUERADE && echo 0 | sudo tee /proc/sys/net/ipv4/ip_forward
+}
+
+function gls() {
+  git log --all --grep="$1"
+}
+
+function ipfwd() {
+  src=$1
+  dst=$2
+  port=$3
+  sudo iptables -t nat -A PREROUTING -p tcp --dport $port -j DNAT --to-destination $dst:$port
+  sudo iptables -t nat -A POSTROUTING -p tcp -d $dst --dport $port -j SNAT --to-source $src
+  sudo bash -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'
+}
+
+function servethis() {
+  IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+  echo "http://$IP:8000"
+  python2.7 -c 'import SimpleHTTPServer; SimpleHTTPServer.test()'
+}
+
+function re() {
+  sudo ifconfig $1 down; sudo ifconfig $1 up
+}
+
+function setpythonenv(){
+    $name=$1
+    export PYTHONPATH=$PYTHONPATH:~/src/pipeline/utils/bkgSubstractor
+    export JUPYTER_PATH=$JUPYTER_PATH:$HOME/src/eval
+    source ~/venvs/$name/bin/activate
+}
--- customize.sh
+++ customize.sh
@@ -108,18 +108,18 @@ then
     export PIPUNISTELLAR
 fi
 
-#update system
-# sudo apt-get update
-# sudo apt-get upgrade
+update system
+sudo apt-get update
+sudo apt-get upgrade -y
 
 
 #install default packages
 PKGS=`cat install-packages/default-packages.list`
 
-# for i in $PKGS
-# do
-#     sudo apt-get install -y $i
-# done
+for i in $PKGS
+do
+    sudo apt-get install -y $i
+done
 
 CONFS=`ls -p config-packages | grep -v /`
 
@@ -132,70 +132,116 @@ do
     fi
 done
 
+#install optional packages
+if [[ $OPTIONAL == 1 ]]
+then
+    PKGS=`cat install-packages/optional-packages.list`
 
-# if [ ! -z "$DISPLAY" ]
-# then
-# 	sudo apt-get -qq -y install $(cat pkg-x.list)
-# 	mkdir -p ~/.config/terminator
-# 	[ -f ~/.config/terminator/config ] && rm ~/.config/terminator/config
-# 	[ ! -h  ~/.config/terminator/config ] && ln -s $PWD/terminator.config ~/.config/terminator/config
-# 	sudo mkdir -p /root/.config/terminator
-# 	sudo [ -f /root/.config/terminator/config ] && sudo rm /root/.config/terminator/config
-# 	sudo [ ! -h /root/.config/terminator/config ] && sudo ln -s $PWD/terminator.config /root/.config/terminator/config
-# fi
+    for i in $PKGS
+    do
+        sudo apt-get install -y $i
+    done
 
-# #HTOP
-# mkdir -p ~/.config/htop
-# sudo mkdir -p /root/.config/htop
-# [ -f ~/.config/htop/htoprc ] && rm ~/.config/htop/htoprc
-# [ ! -h ~/.config/htop/htoprc ] && ln -s $PWD/htoprc ~/.config/htop/htoprc
-# sudo [ -f /root/.config/htop/htoprc ] && sudo rm /root/.config/htop/htoprc
-# sudo [ ! -h /root/.config/htop/htoprc ] && sudo ln -s $PWD/htoprc /root/.config/htop/htoprc
+    CONFS=`ls -p config-packages | grep -v /`
 
-# #VIM
-# [ -f ~/.vimrc ] && rm ~/.vimrc
-# [ ! -h ~/.vimrc ] && ln -s $PWD/vimrc ~/.vimrc
-# sudo [ -f /root/.vimrc ] && sudo rm /root/.vimrc
-# sudo [ ! -h /root/.vimrc ] && sudo ln -s $PWD/vimrc /root/.vimrc
+    for i in $CONFS
+    do
+        if [[ `echo $PKGS | grep -w "$i"` ]]
+        then
+            var="bash config-packages/$i"
+            eval $var
+        fi
+    done
+fi
 
-# #FONT SOURCE CODE PRO
-# FONT_HOME=~/.fonts # actually this path is better : ~/.local/share/fonts
-# FONT_ROOT=/root/.local/share/fonts
+#install unistellar packages
+if [[ $UNISTELLAR == 1 ]]
+then
 
-# mkdir -p "$FONT_HOME/adobe-fonts/source-code-pro"
-# sudo mkdir -p "$FONT_ROOT/adobe-fonts/source-code-pro"
+    PKGS=`cat install-packages/unistellar-packages.list`
 
-# (git clone \
-#    --branch release \
-#    --depth 1 \
-#    'https://github.com/adobe-fonts/source-code-pro.git' \
-#    "$FONT_HOME/adobe-fonts/source-code-pro" && \
-# fc-cache -f -v "$FONT_HOME/adobe-fonts/source-code-pro")
+    for i in $PKGS
+    do
+        sudo apt-get install -y $i
+    done
 
-# (sudo git clone \
-#    --branch release \
-#    --depth 1 \
-#    'https://github.com/adobe-fonts/source-code-pro.git' \
-#    "$FONT_ROOT/adobe-fonts/source-code-pro" && \
-# sudo fc-cache -f -v "$FONT_ROOT/adobe-fonts/source-code-pro")
+    CONFS=`ls -p config-packages | grep -v /`
+
+    for i in $CONFS
+    do
+        if [[ `echo $PKGS | grep -w "$i"` ]]
+        then
+            var="bash config-packages/$i"
+            eval $var
+        fi
+    done
+fi
+
+#FONT SOURCE CODE PRO
+FONT_HOME=~/.local/share/fonts
+
+mkdir -p "$FONT_HOME/adobe-fonts/source-code-pro"
+
+(git clone \
+   --branch release \
+   --depth 1 \
+   'https://github.com/adobe-fonts/source-code-pro.git' \
+   "$FONT_HOME/adobe-fonts/source-code-pro" && \
+fc-cache -f -v "$FONT_HOME/adobe-fonts/source-code-pro")
+
+if [[ $ONEUSER == 1 ]]
+then
+    FONT_ROOT=/root/.local/share/fonts
+    sudo mkdir -p "$FONT_ROOT/adobe-fonts/source-code-pro"
+
+    (sudo git clone \
+          --branch release \
+          --depth 1 \
+          'https://github.com/adobe-fonts/source-code-pro.git' \
+          "$FONT_ROOT/adobe-fonts/source-code-pro" && \
+         sudo fc-cache -f -v "$FONT_ROOT/adobe-fonts/source-code-pro")
+fi
+
+[ ! -h ~/.bash_command_timer.sh ] && ln -s $PWD/bash_command_timer.sh ~/.bash_command_timer.sh
+if [[ $ONEUSER == 1 ]]
+then
+    sudo [ ! -h /root/.bash_command_timer.sh ] && sudo ln -s $PWD/bash_command_timer.sh /root/.bash_command_timer.sh
+fi
 
 # #BASH
-# [ ! -h ~/.aliases.sh ] && ln -s $PWD/aliases.sh ~/.aliases.sh
-# sudo [ ! -h /root/.aliases.sh ] && sudo ln -s $PWD/aliases.sh /root/.aliases.sh
+[ -f ~/.aliases.sh ] && rm ~/.aliases.sh
+[ ! -h ~/.aliases.sh ] && ln -s $PWD/config/aliases.sh ~/.aliases.sh
+if [[ $ONEUSER == 1 ]]
+then
+    sudo [ -f /root/.aliases.sh ] && sudo rm /root/.aliases.sh
+    sudo [ ! -h /root/.aliases.sh ] && sudo ln -s $PWD/config/aliases.sh /root/.aliases.sh
+fi
 
-# [ ! -h ~/.bash_command_timer.sh ] && ln -s $PWD/bash_command_timer.sh ~/.bash_command_timer.sh
-# sudo [ ! -h /root/.bash_command_timer.sh ] && sudo ln -s $PWD/bash_command_timer.sh /root/.bash_command_timer.sh
+if [[ $UNISTELLAR == 1 ]]
+then
+    [ -f ~/.aliases-unistellar.sh ] && rm ~/.aliases-unistellar.sh
+    [ ! -h ~/.aliases-unistellar.sh ] && ln -s $PWD/config/aliases-unistellar.sh ~/.aliases-unistellar.sh
 
-# [ -f ~/.bashrc ] && rm ~/.bashrc
-# [ ! -h ~/.bashrc ] && ln -s $PWD/bashrc ~/.bashrc
-# sudo [ -f /root/.bashrc ] && sudo rm /root/.bashrc
-# sudo [ ! -h /root/.bashrc ] && sudo ln -s $PWD/bashrc /root/.bashrc
+    if [[ $ONEUSER == 1 ]]
+    then
+        sudo [ -f /root/.aliases-unistellar.sh ] && sudo rm /root/.aliases-unistellar.sh
+        sudo [ ! -h /root/.aliases-unistellar.sh ] && sudo ln -s $PWD/config/aliases-unistellar.sh /root/.aliases-unistellar.sh
+    fi
+fi
 
-# #APT
-# echo "Dpkg::Progress-Fancy 1;
-# APT::Color 1;" | sudo tee /etc/apt/apt.conf.d/99progressbar > /dev/null
+[ -f ~/.bashrc ] && rm ~/.bashrc
+[ ! -h ~/.bashrc ] && ln -s $PWD/config/bashrc ~/.bashrc
+if [[ $ONEUSER == 1 ]]
+then
+    sudo [ -f /root/.bashrc ] && sudo rm /root/.bashrc
+    sudo [ ! -h /root/.bashrc ] && sudo ln -s $PWD/config/bashrc /root/.bashrc
+fi
 
-# sudo apt-file update
+#APT
+echo "Dpkg::Progress-Fancy 1;
+APT::Color 1;" | sudo tee /etc/apt/apt.conf.d/99progressbar > /dev/null
 
-# echo "Ready!"
+sudo apt-file update
+
+echo "Ready!"
 # bash
